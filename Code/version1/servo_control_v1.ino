c043e64423f481b81ef65967a03a2e5d6ebed95f
Kevin Ravakhah <144391934+kevin-rav@users.noreply.github.com>
2024-04-04 18:15:56 -0400

    Fix finger delay

--- Code/version1/servo_control_v1.ino
+++ Code/version1/servo_control_v1.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-#define NUM_SERVOS 1 // 5 servos for each finger
+#define NUM_SERVOS 2 // 5 servos for each finger
 
 Servo servos[NUM_SERVOS]; // create servo objects
-int servoPins[NUM_SERVOS] = {9}; // pin locations
-int servoAngles[NUM_SERVOS] = {0}; // angle of each servo 
+int servoPins[NUM_SERVOS] = {8,9}; // pin locations
+int servoAngles[NUM_SERVOS] = {0,0}; // angle of each servo 
 
 void setup() {
   for (int i = 0; i < NUM_SERVOS; i++) {
@@ -33,10 +33,13 @@ void moveServo(int servoIndex, int position) {
 void pow() {
   for (int i = 0; i < NUM_SERVOS; i++) {
     moveServo(i, 0);
-    delay(1000);
   }
+
+  delay(1000);
+
   for (int i = 0; i < NUM_SERVOS; i++) {
     moveServo(i, 180);
-    delay(1000);
   }
+
+  delay(1000);
 }
